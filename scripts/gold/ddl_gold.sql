be3cbfad3f8e800e2873fc58b9487720a4beb536
Fares <faresflash14@gmail.com>
2026-04-27 12:02:00 +0300

    fixed some bugs

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -46,7 +46,7 @@ CREATE VIEW gold.dim_products AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate key
     pn.prd_id       AS product_id,
-    pn.prd_key      AS product_number,
+    pn.sls_prd_key      AS product_number,
     pn.prd_nm       AS product_name,
     pn.cat_id       AS category_id,
     pc.cat          AS category,
@@ -61,9 +61,9 @@ LEFT JOIN silver.erp_px_cat_g1v2 pc
 WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
 GO
 
--- =============================================================================
+
 -- Create Fact Table: gold.fact_sales
--- =============================================================================
+
 IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
     DROP VIEW gold.fact_sales;
 GO
